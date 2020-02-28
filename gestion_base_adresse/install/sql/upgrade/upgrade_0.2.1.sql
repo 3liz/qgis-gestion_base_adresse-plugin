fa7178f8177c9c7e2ef4fd0be7a6994b139ac4d5
Michael Douchin <mdouchin@3liz.com>
2020-02-28 16:43:34 +0100

    Database - Rename 4 columns of point_adresse

---
+++ gestion_base_adresse/install/sql/upgrade/upgrade_0.2.1.sql
@@ -0,0 +1,5 @@
+ALTER TABLE adresse.point_adresse RENAME COLUMN date_modif_point TO date_modif;
+ALTER TABLE adresse.point_adresse RENAME COLUMN createur_point TO createur;
+ALTER TABLE adresse.point_adresse RENAME COLUMN modificateur_point TO modificateur;
+ALTER TABLE adresse.point_adresse RENAME COLUMN date_creation_point TO date_creation;
+
