27b4a8225b021d3d8b4b53376ebf5f97617fd761
Michael Douchin <mdouchin@3liz.com>
2020-02-28 12:04:13 +0100

    Installation - Ajout des scripts SQL et outil de génération automatique

---
+++ install/sql/adresse/50_TRIGGER.sql
@@ -0,0 +1,34 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.16
+-- Dumped by pg_dump version 9.6.16
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- point_adresse nb_point
+CREATE TRIGGER nb_point AFTER INSERT ON adresse.point_adresse FOR EACH ROW EXECUTE PROCEDURE adresse.calcul_point_voie();
+
+
+-- voie nom_complet
+CREATE TRIGGER nom_complet BEFORE INSERT ON adresse.voie FOR EACH ROW EXECUTE PROCEDURE adresse.voie_nom_complet();
+
+
+-- point_adresse trigger_point_adr
+CREATE TRIGGER trigger_point_adr BEFORE INSERT ON adresse.point_adresse FOR EACH ROW EXECUTE PROCEDURE adresse.trigger_point_adr();
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ install/sql/adresse/60_CONSTRAINT.sql
@@ -0,0 +1,104 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.16
+-- Dumped by pg_dump version 9.6.16
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+-- appartenir_com appartenir_com_pkey
+ALTER TABLE ONLY adresse.appartenir_com
+    ADD CONSTRAINT appartenir_com_pkey PRIMARY KEY (id_voie, id_com);
+
+
+-- commune_deleguee commune_deleguee_pkey
+ALTER TABLE ONLY adresse.commune_deleguee
+    ADD CONSTRAINT commune_deleguee_pkey PRIMARY KEY (id_com_del);
+
+
+-- commune commune_pkey
+ALTER TABLE ONLY adresse.commune
+    ADD CONSTRAINT commune_pkey PRIMARY KEY (id_com);
+
+
+-- document document_pkey
+ALTER TABLE ONLY adresse.document
+    ADD CONSTRAINT document_pkey PRIMARY KEY (id_doc);
+
+
+-- parcelle parcelle_pkey
+ALTER TABLE ONLY adresse.parcelle
+    ADD CONSTRAINT parcelle_pkey PRIMARY KEY (fid);
+
+
+-- point_adresse point_adresse_pkey
+ALTER TABLE ONLY adresse.point_adresse
+    ADD CONSTRAINT point_adresse_pkey PRIMARY KEY (id_point);
+
+
+-- referencer_com referencer_com_pkey
+ALTER TABLE ONLY adresse.referencer_com
+    ADD CONSTRAINT referencer_com_pkey PRIMARY KEY (id_com, id_com_deleguee);
+
+
+-- voie voie_pkey
+ALTER TABLE ONLY adresse.voie
+    ADD CONSTRAINT voie_pkey PRIMARY KEY (id_voie);
+
+
+-- appartenir_com appartenir_com_id_com_fkey
+ALTER TABLE ONLY adresse.appartenir_com
+    ADD CONSTRAINT appartenir_com_id_com_fkey FOREIGN KEY (id_com) REFERENCES adresse.commune(id_com);
+
+
+-- appartenir_com appartenir_com_id_voie_fkey
+ALTER TABLE ONLY adresse.appartenir_com
+    ADD CONSTRAINT appartenir_com_id_voie_fkey FOREIGN KEY (id_voie) REFERENCES adresse.voie(id_voie);
+
+
+-- document document_id_commune_fkey
+ALTER TABLE ONLY adresse.document
+    ADD CONSTRAINT document_id_commune_fkey FOREIGN KEY (id_commune) REFERENCES adresse.commune(id_com);
+
+
+-- point_adresse point_adresse_id_commune_fkey
+ALTER TABLE ONLY adresse.point_adresse
+    ADD CONSTRAINT point_adresse_id_commune_fkey FOREIGN KEY (id_commune) REFERENCES adresse.commune(id_com);
+
+
+-- point_adresse point_adresse_id_parcelle_fkey
+ALTER TABLE ONLY adresse.point_adresse
+    ADD CONSTRAINT point_adresse_id_parcelle_fkey FOREIGN KEY (id_parcelle) REFERENCES adresse.parcelle(fid);
+
+
+-- point_adresse point_adresse_id_voie_fkey
+ALTER TABLE ONLY adresse.point_adresse
+    ADD CONSTRAINT point_adresse_id_voie_fkey FOREIGN KEY (id_voie) REFERENCES adresse.voie(id_voie);
+
+
+-- referencer_com referencer_com_id_com_deleguee_fkey
+ALTER TABLE ONLY adresse.referencer_com
+    ADD CONSTRAINT referencer_com_id_com_deleguee_fkey FOREIGN KEY (id_com_deleguee) REFERENCES adresse.commune_deleguee(id_com_del);
+
+
+-- referencer_com referencer_com_id_com_fkey
+ALTER TABLE ONLY adresse.referencer_com
+    ADD CONSTRAINT referencer_com_id_com_fkey FOREIGN KEY (id_com) REFERENCES adresse.commune(id_com);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
