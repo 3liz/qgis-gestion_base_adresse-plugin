a3c6965999245594a6c5b5ff54016f579ab487ad
Michael Douchin <mdouchin@3liz.com>
2020-03-02 10:41:02 +0100

    Database - Add spatial indexes

--- gestion_base_adresse/install/sql/adresse/40_INDEX.sql
+++ gestion_base_adresse/install/sql/adresse/40_INDEX.sql
@@ -16,6 +16,28 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+-- commune_deleguee_geom_idx
+CREATE INDEX commune_deleguee_geom_idx ON adresse.commune_deleguee USING gist (geom);
+
+
+-- commune_geom_idx
+CREATE INDEX commune_geom_idx ON adresse.commune USING gist (geom);
+
+
+-- parcelle_geom_idx
+CREATE INDEX parcelle_geom_idx ON adresse.parcelle USING gist (geom);
+
+
+-- point_adresse_geom_idx
+CREATE INDEX point_adresse_geom_idx ON adresse.point_adresse USING gist (geom);
+
+
+-- voie_geom_idx
+CREATE INDEX voie_geom_idx ON adresse.voie USING gist (geom);
+
+
 --
 -- PostgreSQL database dump complete
 --
