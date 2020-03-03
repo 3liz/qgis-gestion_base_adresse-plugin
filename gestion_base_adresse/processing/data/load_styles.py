ffe886596886e686174420ac2ff2210fac7b399e
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-03-03 11:20:24 +0100

    code cleanup in load_styles.py

--- gestion_base_adresse/processing/data/load_styles.py
+++ gestion_base_adresse/processing/data/load_styles.py
@@ -3,72 +3,40 @@ __license__ = 'GPL version 3'
 __email__ = 'info@3liz.org'
 __revision__ = '$Format:%H$'
 
-from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (
     QgsProcessingParameterString,
-    QgsProcessingOutputMultipleLayers,
     QgsProcessingOutputString,
-    QgsProcessingAlgorithm,
-    QgsProject,
-    QgsDataSourceUri,
-    QgsProcessingContext,
-    QgsVectorLayer,
     QgsProcessingParameterDefinition,
 )
-from processing.tools.postgis import uri_from_name, GeoDB
+from ...qgis_plugin_tools.tools.algorithm_processing import BaseProcessingAlgorithm
+from ...qgis_plugin_tools.tools.i18n import tr
 from ...qgis_plugin_tools.tools.resources import resources_path
 
 
-class LoadStylesAlgorithm(QgsProcessingAlgorithm):
+class LoadStylesAlgorithm(BaseProcessingAlgorithm):
     """
     Chargement des couches adresse depuis la base de données
     """
-
-    # Constants used to refer to parameters and outputs. They will be
-    # used when calling the algorithm from another algorithm, or when
-    # calling from the QGIS console.
 
     INPUT = 'INPUT'
     OUTPUT_MSG = 'OUTPUT MSG'
 
     def name(self):
-        """
-        Returns the algorithm name, used for identifying the algorithm.
-        """
         return 'load_styles'
 
     def displayName(self):
-        """
-        Returns the translated algorithm name, which should be used for any
-        user-visible display of the algorithm name.
-        """
-        return self.tr('Chargement des styles depuis le dossier resources')
+        return tr('Chargement des styles depuis le dossier resources')
 
     def groupId(self):
-        """
-        Returns the unique ID of the group this algorithm belongs to.
-        """
         return 'adresse_donnees'
 
     def group(self):
-        """
-        Returns the name of the group this algorithm belongs to. This string
-        should be localised.
-        """
-        return self.tr('Données')
+        return tr('Données')
 
-    def tr(self, string):
-        return QCoreApplication.translate('Processing', string)
-
-    def createInstance(self):
-        return LoadStylesAlgorithm()
+    def shortHelpString(self):
+        return tr('Charger les styles pour les différentes couches.')
 
     def initAlgorithm(self, config):
-        """
-        Here we define the inputs and output of the algorithm, along
-        with some other properties.
-        """
-
         # INPUTS
         parameter = QgsProcessingParameterString(
             self.INPUT, 'Champ qui ne sert à rien !', optional=True)
@@ -79,14 +47,13 @@ class LoadStylesAlgorithm(QgsProcessingAlgorithm):
         self.addOutput(
             QgsProcessingOutputString(
                 self.OUTPUT_MSG,
-                self.tr('Message de sortie')
+                tr('Message de sortie')
             )
         )
 
 
     def processAlgorithm(self, parameters, context, feedback):
         msg = ''
-        output_layers = []
         layers_name = ['commune', 'voie', 'point_adresse', 'parcelle']
 
         for x in layers_name:
@@ -94,20 +61,8 @@ class LoadStylesAlgorithm(QgsProcessingAlgorithm):
             if layer:
                 for l in layer:
                     feedback.pushInfo(l.name() + ', qml loaded')
-                    l.loadNamedStyle(resources_path('qml', x+'.qml'))
-                    feedback.pushInfo('Style for ' + x +' successfully loaded')
-                    msg = msg+ ' // Style for ' + x +' successfully loaded'
+                    l.loadNamedStyle(resources_path('qml', x + '.qml'))
+                    feedback.pushInfo('Style for ' + x + ' successfully loaded')
+                    msg = msg + ' // Style for ' + x + ' successfully loaded'
 
-
-
-        """
-        Here is where the processing itself takes place.
-        """
-
-        # Return the results of the algorithm. In this case our only result is
-        # the feature sink which contains the processed features, but some
-        # algorithms may return multiple feature sinks, calculated numeric
-        # statistics, etc. These should all be included in the returned
-        # dictionary, with keys matching the feature corresponding parameter
-        # or output names.
         return {self.OUTPUT_MSG: msg}
