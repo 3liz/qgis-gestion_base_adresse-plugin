82298383623b40e96c8855dc43d0d53563213977
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-03-11 17:45:36 +0100

    add PostgreSQL and PostGIS version in test logs

--- docker/script.sh
+++ docker/script.sh
@@ -7,8 +7,6 @@ echo 'Wait 10 seconds'
 sleep 10
 
 echo 'Installation of the plugin'
-# docker exec -it qgis rm -f  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/gestion_base_adresse
-# docker exec -it qgis ln -s /gestion_base_adresse/ /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/gestion_base_adresse
 docker exec -it qgis sh -c "qgis_setup.sh gestion_base_adresse"
 docker cp postgis_connexions.ini qgis:/tmp
 docker exec qgis bash -c "cat /tmp/postgis_connexions.ini >> /root/.local/share/QGIS/QGIS3/profiles/default/QGIS/QGIS3.ini"
