d88c95e94a641f471e1be37252a49fe42bade492
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-03-12 09:31:13 +0100

    add unittest base class for testing database

---
+++ gestion_base_adresse/test/database_test.py
@@ -0,0 +1,58 @@
+"""Base class for tests using a database with data."""
+
+import psycopg2
+
+from qgis.core import (
+    QgsApplication,
+    Qgis,
+)
+from qgis.testing import unittest
+
+if Qgis.QGIS_VERSION_INT >= 30800:
+    from qgis import processing
+else:
+    import processing
+
+from ..processing.provider import GestionAdresseProvider
+from ..qgis_plugin_tools.tools.logger_processing import LoggerProcessingFeedBack
+
+__copyright__ = 'Copyright 2019, 3Liz'
+__license__ = 'GPL version 3'
+__email__ = 'info@3liz.org'
+__revision__ = '$Format:%H$'
+
+
+
+class DatabaseTestCase(unittest.TestCase):
+
+    """Base class for tests using a database with data."""
+
+    def setUp(self) -> None:
+        self.connection = psycopg2.connect(
+            user='docker',
+            password='docker',
+            host='db',
+            port='5432',
+            database='gis'
+        )
+        self.cursor = self.connection.cursor()
+
+        self.provider = GestionAdresseProvider()
+        QgsApplication.processingRegistry().addProvider(self.provider)
+
+        self.feedback = LoggerProcessingFeedBack()
+
+        params = {
+            'CONNECTION_NAME': 'test',
+            'OVERRIDE': True,
+            'ADDTESTDATA': True,
+        }
+        processing.run('gestion_adresse:create_database_structure', params, feedback=None)
+
+        params = {
+            'CONNECTION_NAME': 'test',
+            'RUNIT': True,
+        }
+        processing.run('gestion_adresse:upgrade_database_structure', params, feedback=None)
+
+        super().setUp()
