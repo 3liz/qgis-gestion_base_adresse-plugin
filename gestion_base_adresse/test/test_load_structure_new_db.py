8aba413bf3ee075477cd08a33a8e8f145e86f853
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-03-12 09:31:13 +0100

    add unittest base class for testing database

---
+++ gestion_base_adresse/test/test_load_structure_new_db.py
@@ -0,0 +1,27 @@
+"""Tests for load structure with an empty database."""
+
+from .database_test import DatabaseTestCase
+
+__copyright__ = 'Copyright 2019, 3Liz'
+__license__ = 'GPL version 3'
+__email__ = 'info@3liz.org'
+__revision__ = '$Format:%H$'
+
+
+
+class TestLoadStructureEmptyDatabase(DatabaseTestCase):
+
+    """This class is redundant with test_load_structure,
+    but this one is using a setup function."""
+
+    def test_configure_project_with_new_db(self):
+        """Test we can load the PostGIS structure using the setup function."""
+
+        self.cursor.execute('SELECT table_name FROM information_schema.tables WHERE table_schema = \'adresse\'')
+        records = self.cursor.fetchall()
+        result = [r[0] for r in records]
+        expected = [
+            'appartenir_com', 'commune', 'document', 'metadata', 'point_adresse',
+            'voie', 'parcelle', 'commune_deleguee', 'referencer_com',
+        ]
+        self.assertCountEqual(expected, result)
