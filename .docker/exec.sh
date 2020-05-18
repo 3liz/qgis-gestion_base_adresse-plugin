6c8e8e846188c096c8137f81d96e7bc51410fc07
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-05-18 22:07:43 +0200

    use loop for SQL files, use etrimaille/schemaspy image

--- .docker/exec.sh
+++ .docker/exec.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker exec -it qgis sh -c "cd /tests_directory/gestion_base_adresse && qgis_testrunner.sh qgis_plugin_tools.infrastructure.test_runner.test_package"
+docker exec -it qgis sh \
+  -c "cd /tests_directory/gestion_base_adresse && qgis_testrunner.sh qgis_plugin_tools.infrastructure.test_runner.test_package"
--- .docker/install_db.sh
+++ .docker/install_db.sh
@@ -4,10 +4,7 @@ docker cp pg_service.conf qgis:/etc/postgresql-common/
 
 echo 'Installation from latest version'
 docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/00_initialize_database.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/10_FUNCTION.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/20_TABLE_SEQUENCE_DEFAULT.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/30_VIEW.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/40_INDEX.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/50_TRIGGER.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/60_CONSTRAINT.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/70_COMMENT.sql" > /dev/null
+for sql_file in `ls -v ../gestion_base_adresse/install/sql/adresse/*.sql`; do
+  echo "${sql_file}"
+  docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/${sql_file}" > /dev/null;
+  done;
