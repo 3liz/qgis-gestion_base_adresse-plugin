c4764904db83d444ba387f4ec5a4a98a0b49f4d2
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-05-18 18:51:53 +0200

    add schemaspy Makefile command

--- .docker/reformat_sql_install.sh
+++ .docker/reformat_sql_install.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-docker cp pg_service.conf qgis:/etc/postgresql-common/
 docker exec qgis bash -c "apt-get install -y rename" > /dev/null
-
-echo 'Installation from latest version'
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/00_initialize_database.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/10_FUNCTION.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/20_TABLE_SEQUENCE_DEFAULT.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/30_VIEW.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/40_INDEX.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/50_TRIGGER.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/60_CONSTRAINT.sql" > /dev/null
-docker exec qgis bash -c "psql service=adresse -f /tests_directory/gestion_base_adresse/install/sql/adresse/70_COMMENT.sql" > /dev/null
 
 echo 'Generating SQL files'
 docker exec qgis bash -c "cd /tests_directory/gestion_base_adresse/install/sql/ && ./export_database_structure_to_SQL.sh adresse adresse"
