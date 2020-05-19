f6687439b69002e6943b2273377c1cc9619ba2f3
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-05-19 18:13:08 +0200

    add missing variable

--- .docker/install_db.sh
+++ .docker/install_db.sh
@@ -6,7 +6,7 @@ docker cp pg_service.conf qgis:/etc/postgresql-common/
 echo 'Installation from latest version'
 docker exec qgis bash -c "psql service=test -c 'DROP SCHEMA IF EXISTS ${SCHEMA} CASCADE;'" > /dev/null
 docker exec qgis bash -c "psql service=test -f /tests_directory/${PLUGIN_NAME}/install/sql/00_initialize_database.sql" > /dev/null
-for sql_file in `ls -v ../${PLUGIN_NAME}/install/sql/adresse/*.sql`; do
+for sql_file in `ls -v ../${PLUGIN_NAME}/install/sql/${SCHEMA}/*.sql`; do
   echo "${sql_file}"
   docker exec qgis bash -c "psql service=test -f /tests_directory/${PLUGIN_NAME}/${sql_file}" > /dev/null;
   done;
