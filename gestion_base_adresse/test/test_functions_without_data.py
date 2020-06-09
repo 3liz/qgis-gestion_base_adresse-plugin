e7839726237d4aec4976a8c37695c8a245da363f
Drillin <sio.drillin@gmail.com>
2020-06-09 13:53:58 +0200

    adding more test

--- gestion_base_adresse/test/test_functions_without_data.py
+++ gestion_base_adresse/test/test_functions_without_data.py
@@ -11,8 +11,12 @@ __revision__ = "$Format:%H$"
 class TestSqlFunctions(DatabaseWithoutDataTestCase):
 
     def test_calcul_segment_proche(self):
-        """ Test the database is provided without data. """
-        # Number of tables
+        """ Test the function calcul_segment_proche """
+        # Test avec un point qui aura le troisième point de la ligne
+        # Comme premier point du segment et le quatrième en dernier
+        # point du segment
+
+        # Test premier point segment
         self.cursor.execute(
             "SELECT ST_astext(ST_startPoint(adresse.calcul_segment_proche(st_geomfromtext"
             "('LINESTRING(429172 6920701, 429165 6920737, 429142 6920823,"
@@ -21,6 +25,20 @@ class TestSqlFunctions(DatabaseWithoutDataTestCase):
         )
         self.assertEqual('POINT(429142 6920823)', self.cursor.fetchone()[0])
 
+        # Test dernier point segment
+        self.cursor.execute(
+            "SELECT ST_astext(ST_endPoint(adresse.calcul_segment_proche(st_geomfromtext"
+            "('LINESTRING(429172 6920701, 429165 6920737, 429142 6920823,"
+            " 429141 6920875, 429147 6920926)', 2154), "
+            "St_geomfromtext('POINT(429162 6920851)', 2154)))) "
+        )
+        self.assertEqual('POINT(429141 6920875)', self.cursor.fetchone()[0])
+
+        # Test avec un point qui aura le quatrieme point de la ligne
+        # Comme premier point du segment et le cinduième en dernier
+        # point du segment
+
+        # Test premier point segment
         self.cursor.execute(
             "SELECT ST_astext(ST_startPoint(adresse.calcul_segment_proche(st_geomfromtext"
             "('LINESTRING(429172 6920701, 429165 6920737, 429142 6920823,"
@@ -28,3 +46,12 @@ class TestSqlFunctions(DatabaseWithoutDataTestCase):
             "St_geomfromtext('POINT(429125 6920901)', 2154)))) "
         )
         self.assertEqual('POINT(429141 6920875)', self.cursor.fetchone()[0])
+
+        # Test dernier point segment
+        self.cursor.execute(
+            "SELECT ST_astext(ST_endPoint(adresse.calcul_segment_proche(st_geomfromtext"
+            "('LINESTRING(429172 6920701, 429165 6920737, 429142 6920823,"
+            " 429141 6920875, 429147 6920926)', 2154), "
+            "St_geomfromtext('POINT(429125 6920901)', 2154)))) "
+        )
+        self.assertEqual('POINT(429147 6920926)', self.cursor.fetchone()[0])
