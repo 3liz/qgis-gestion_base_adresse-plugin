2c8961d01d2fc6e941b63caf6746e772a0691418
Drillin <sio.drillin@gmail.com>
2020-07-17 15:26:55 +0200

    Update function trigger calcul_point_voie

--- gestion_base_adresse/install/sql/adresse/10_FUNCTION.sql
+++ gestion_base_adresse/install/sql/adresse/10_FUNCTION.sql
@@ -210,9 +210,21 @@ BEGIN
     IF TG_OP = 'DELETE' THEN
         SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = OLD.id_voie;
         UPDATE adresse.voie SET nb_point = nb WHERE id_voie = OLD.id_voie;
-    ELSIF TG_OP = 'INSERT' THEN
+    ELSIF TG_OP = 'INSERT'  THEN
+      IF NEW.id_voie IS NOT NULL THEN
         SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = NEW.id_voie;
         UPDATE adresse.voie SET nb_point = nb WHERE id_voie = NEW.id_voie;
+      END IF;
+    ELSIF TG_OP = 'UPDATE' AND NEW.id_voie IS DISTINCT FROM OLD.id_voie THEN
+      IF NEW.id_voie IS NULL THEN
+        SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = OLD.id_voie;
+        UPDATE adresse.voie SET nb_point = nb WHERE id_voie = OLD.id_voie;
+      ELSE
+        SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = NEW.id_voie;
+        UPDATE adresse.voie SET nb_point = nb WHERE id_voie = NEW.id_voie;
+        SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = OLD.id_voie;
+        UPDATE adresse.voie SET nb_point = nb WHERE id_voie = OLD.id_voie;
+      END IF;
     END IF;
 
     RETURN NEW;
@@ -476,4 +488,3 @@ $$;
 --
 -- PostgreSQL database dump complete
 --
-
--- gestion_base_adresse/install/sql/upgrade/upgrade_to_0.5.0.sql
+++ gestion_base_adresse/install/sql/upgrade/upgrade_to_0.5.0.sql
@@ -1,5 +1,35 @@
 BEGIN;
 
+CREATE OR REPLACE FUNCTION adresse.calcul_point_voie() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    nb integer;
+BEGIN
+    IF TG_OP = 'DELETE' THEN
+        SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = OLD.id_voie;
+        UPDATE adresse.voie SET nb_point = nb WHERE id_voie = OLD.id_voie;
+    ELSIF TG_OP = 'INSERT'  THEN
+      IF NEW.id_voie IS NOT NULL THEN
+        SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = NEW.id_voie;
+        UPDATE adresse.voie SET nb_point = nb WHERE id_voie = NEW.id_voie;
+      END IF;
+    ELSIF TG_OP = 'UPDATE' AND NEW.id_voie IS DISTINCT FROM OLD.id_voie THEN
+      IF NEW.id_voie IS NULL THEN
+        SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = OLD.id_voie;
+        UPDATE adresse.voie SET nb_point = nb WHERE id_voie = OLD.id_voie;
+      ELSE
+        SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = NEW.id_voie;
+        UPDATE adresse.voie SET nb_point = nb WHERE id_voie = NEW.id_voie;
+        SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = OLD.id_voie;
+        UPDATE adresse.voie SET nb_point = nb WHERE id_voie = OLD.id_voie;
+      END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
 DROP TRIGGER IF EXISTS nb_point ON adresse.point_adresse;
 CREATE TRIGGER nb_point
     AFTER INSERT OR UPDATE
