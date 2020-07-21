abdc70967eeadfd3ff774ee048901d48e1b3bd9e
Drillin <sio.drillin@gmail.com>
2020-07-21 15:06:19 +0200

    Apply suggestions from code review
    Co-authored-by: rldhont <rldhont@gmail.com>

--- gestion_base_adresse/install/sql/adresse/10_FUNCTION.sql
+++ gestion_base_adresse/install/sql/adresse/10_FUNCTION.sql
@@ -219,6 +219,9 @@ BEGIN
       IF NEW.id_voie IS NULL THEN
         SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = OLD.id_voie;
         UPDATE adresse.voie SET nb_point = nb WHERE id_voie = OLD.id_voie;
+      ELSIF OLD.id_voie IS NULL THEN
+          SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = NEW.id_voie;
+          UPDATE adresse.voie SET nb_point = nb WHERE id_voie = NEW.id_voie;
       ELSE
         SELECT COUNT(id_point) into nb FROM adresse.point_adresse WHERE id_voie = NEW.id_voie;
         UPDATE adresse.voie SET nb_point = nb WHERE id_voie = NEW.id_voie;
@@ -488,4 +491,3 @@ $$;
 --
 -- PostgreSQL database dump complete
 --
-
