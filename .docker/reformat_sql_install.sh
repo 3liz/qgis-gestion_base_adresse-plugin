c3cbeff54bd8d74fc0bab98c83d94f152bfd57da
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-09-16 09:46:40 +0200

    Tests - Check git is clean before making the command reformat_sql

--- .docker/reformat_sql_install.sh
+++ .docker/reformat_sql_install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 export $(grep -v '^#' .env | xargs)
 
+if [ -n "$(git status --porcelain -uno)" ];
+then
+    echo "Git working directory is not clean. Aborting."
+    exit 1
+fi
+
 docker exec postgis bash -c "apt-get install -y rename" > /dev/null
 
 echo 'Generating SQL files'
