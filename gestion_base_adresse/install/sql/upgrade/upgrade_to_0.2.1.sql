df39c3ee8bb2e4338155fbea749480d232cd05b3
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-09-16 09:50:35 +0200

    Tests - Remove old upgrade files to not interfer in tests

--- gestion_base_adresse/install/sql/upgrade/upgrade_to_0.2.1.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-
-ALTER TABLE adresse.point_adresse RENAME COLUMN date_modif_point TO date_modif;
-ALTER TABLE adresse.point_adresse RENAME COLUMN createur_point TO createur;
-ALTER TABLE adresse.point_adresse RENAME COLUMN modificateur_point TO modificateur;
-ALTER TABLE adresse.point_adresse RENAME COLUMN date_creation_point TO date_creation;
-
-COMMIT;
