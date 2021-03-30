3df2e7e7f2fce5acf0e45206803918ae42ca9ee6
Jérémie Ory <ory.jeremie@gmail.com>
2021-03-30 14:07:51 +0200

    Update 30_VIEW.sql
    Update de l'export BAL -> BAL 1.2

--- gestion_base_adresse/install/sql/adresse/30_VIEW.sql
+++ gestion_base_adresse/install/sql/adresse/30_VIEW.sql
@@ -1,3 +1,4 @@
+
 --
 -- PostgreSQL database dump
 --
@@ -26,34 +27,89 @@ CREATE VIEW adresse.v_commune AS
 
 -- v_export_bal
 CREATE VIEW adresse.v_export_bal AS
- SELECT p.id_point,
+ SELECT ''::text AS uid_adresse,
         CASE
-            WHEN ((p.suffixe IS NOT NULL) AND (v.code_fantoir IS NOT NULL)) THEN concat(c.insee_code, '_', (v.code_fantoir)::text, '_', lpad((p.numero)::text, 5, '0'::text), '_', p.suffixe)
-            WHEN ((p.suffixe IS NULL) AND (v.code_fantoir IS NULL)) THEN concat(c.insee_code, '_', 'xxxx', '_', lpad((p.numero)::text, 5, '0'::text))
-            WHEN ((p.suffixe IS NULL) AND (v.code_fantoir IS NOT NULL)) THEN concat(c.insee_code, '_', (v.code_fantoir)::text, '_', lpad((p.numero)::text, 5, '0'::text))
-            WHEN ((p.suffixe IS NOT NULL) AND (v.code_fantoir IS NULL)) THEN concat(c.insee_code, '_', 'xxxx', '_', lpad((p.numero)::text, 5, '0'::text), '_', p.suffixe)
+            WHEN p.suffixe IS NOT NULL AND v.code_fantoir IS NOT NULL THEN concat(c.insee_code, '_', v.code_fantoir::text, '_', lpad(p.numero::text, 5, '0'::text), '_', p.suffixe)
+            WHEN p.suffixe IS NULL AND v.code_fantoir IS NULL THEN concat(c.insee_code, '_', 'xxxx', '_', lpad(p.numero::text, 5, '0'::text))
+            WHEN p.suffixe IS NULL AND v.code_fantoir IS NOT NULL THEN concat(c.insee_code, '_', v.code_fantoir::text, '_', lpad(p.numero::text, 5, '0'::text))
+            WHEN p.suffixe IS NOT NULL AND v.code_fantoir IS NULL THEN concat(c.insee_code, '_', 'xxxx', '_', lpad(p.numero::text, 5, '0'::text), '_', p.suffixe)
             ELSE NULL::text
         END AS cle_interop,
-    ''::text AS uid_adresse,
+    p.commune_insee,
+    c.commune_nom,
+    p.commune_deleguee_insee,
+    p.commune_deleguee_nom,
     v.nom_complet AS voie_nom,
+    p.lieudit_complement_nom,
     p.numero,
     p.suffixe,
-    c.commune_nom,
     p.type_pos AS "position",
-    public.st_x(p.geom) AS x,
-    public.st_y(p.geom) AS y,
-    public.st_x(public.st_transform(p.geom, 4326)) AS long,
-    public.st_y(public.st_transform(p.geom, 4326)) AS lat,
-    ''::text AS source,
-    p.date_modif AS date_der_maj,
-    c.insee_code AS code_insee
+    st_x(p.geom) AS x,
+    st_y(p.geom) AS y,
+    round(st_x(st_transform(p.geom, 4326))::numeric, 10) AS long,
+    round(st_y(st_transform(p.geom, 4326))::numeric, 10) AS lat,
+    g.geo_parcelle AS cad_parcelles,
+    c.commune_nom AS source,
+    p.date_modif AS date_der_maj
+   FROM adresse.commune c,
+    adresse.point_adresse p,
+    adresse.voie v,
+    cadastre.geo_parcelle g
+  WHERE p.id_commune = c.id_com AND st_intersects(g.geom, p.geom) AND p.id_voie = v.id_voie
+UNION
+ SELECT ''::text AS uid_adresse,
+    concat(commune.insee_code, '_', 'xxxx', '_', ld.numero) AS cle_interop,
+    commune.insee_code AS commune_insee,
+    ld.commune_nom,
+    commune_deleguee.insee_code AS commune_deleguee_insee,
+    ld.commune_deleguee_nom,
+    ld.nom_ld AS voie_nom,
+    ''::character varying(255) AS lieudit_complement_nom,
+    ld.numero,
+    ''::text AS suffixe,
+    ''::text AS "position",
+    st_x(ld.geom) AS x,
+    st_y(ld.geom) AS y,
+    round(st_x(st_transform(ld.geom, 4326))::numeric, 10) AS long,
+    round(st_y(st_transform(ld.geom, 4326))::numeric, 10) AS lat,
+    ''::text AS cad_parcelles,
+    ld.commune_nom AS source,
+    ld.date_der_maj
+   FROM adresse.lieux_dits ld
+     LEFT JOIN adresse.commune_deleguee ON commune_deleguee.id_com_del = ld.id_com_del
+     LEFT JOIN adresse.commune ON commune.id_com = ld.id_com
+  WHERE ld.integration_ban = true
+UNION
+ SELECT ''::text AS uid_adresse,
+        CASE
+            WHEN p.suffixe IS NOT NULL AND v.code_fantoir IS NOT NULL THEN concat(c.insee_code, '_', v.code_fantoir::text, '_', lpad(p.numero::text, 5, '0'::text), '_', p.suffixe)
+            WHEN p.suffixe IS NULL AND v.code_fantoir IS NULL THEN concat(c.insee_code, '_', 'xxxx', '_', lpad(p.numero::text, 5, '0'::text))
+            WHEN p.suffixe IS NULL AND v.code_fantoir IS NOT NULL THEN concat(c.insee_code, '_', v.code_fantoir::text, '_', lpad(p.numero::text, 5, '0'::text))
+            WHEN p.suffixe IS NOT NULL AND v.code_fantoir IS NULL THEN concat(c.insee_code, '_', 'xxxx', '_', lpad(p.numero::text, 5, '0'::text), '_', p.suffixe)
+            ELSE NULL::text
+        END AS cle_interop,
+    p.commune_insee,
+    c.commune_nom,
+    p.commune_deleguee_insee,
+    p.commune_deleguee_nom,
+    v.nom_complet AS voie_nom,
+    p.lieudit_complement_nom,
+    p.numero,
+    p.suffixe,
+    p.type_pos AS "position",
+    st_x(p.geom) AS x,
+    st_y(p.geom) AS y,
+    round(st_x(st_transform(p.geom, 4326))::numeric, 10) AS long,
+    round(st_y(st_transform(p.geom, 4326))::numeric, 10) AS lat,
+    ''::text AS cad_parcelles,
+    c.commune_nom AS source,
+    p.date_modif AS date_der_maj
    FROM adresse.commune c,
     adresse.point_adresse p,
     adresse.voie v
-  WHERE ((p.id_commune = c.id_com) AND (p.id_voie = v.id_voie));
+  WHERE p.id_parcelle IS NULL AND p.id_commune = c.id_com AND p.id_voie = v.id_voie;
 
 
 --
 -- PostgreSQL database dump complete
 --
-
