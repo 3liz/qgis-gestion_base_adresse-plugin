ca97da290392a2f661d684caa0d8bc70a1f925c8
Drillin Pierre <sio.drillin@gmail.com>
2021-07-27 16:51:38 +0200

    Switch point_adresse to v_point_adresse

--- gestion_base_adresse/processing/data/load_styles.py
+++ gestion_base_adresse/processing/data/load_styles.py
@@ -55,7 +55,7 @@ class LoadStylesAlgorithm(BaseProcessingAlgorithm):
     def processAlgorithm(self, parameters, context, feedback):
         _ = parameters
         msg = ""
-        layers_name = ["commune", "voie", "point_adresse", "parcelle", "document"]
+        layers_name = ["commune", "voie", "v_point_adresse", "parcelle", "document"]
 
         for x in layers_name:
             layers = context.project().mapLayersByName(x)
