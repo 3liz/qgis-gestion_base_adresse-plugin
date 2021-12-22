95323ad5444eff6280469b70748ad511b272fcc5
Etienne Trimaille <gustrimaille@yahoo.fr>
2021-12-22 10:47:25 +0100

    SQL - Update export_bal view with St_Contains

--- gestion_base_adresse/install/sql/upgrade/upgrade_to_1.1.0.sql
+++ gestion_base_adresse/install/sql/upgrade/upgrade_to_1.1.0.sql
@@ -24,4 +24,60 @@ $$;
 
 COMMENT ON FUNCTION adresse.reset_referencer_com IS 'Permet la réinitialisation de la table adresse.referencer_com en faisant une intersection entre adresse.commune_deleguee et adresse.commune';
 
+CREATE OR REPLACE VIEW adresse.v_export_bal AS
+ SELECT ''::text AS uid_adresse,
+        CASE
+            WHEN ((p.suffixe IS NOT NULL) AND (v.code_fantoir IS NOT NULL)) THEN concat(c.insee_code, '_', (v.code_fantoir)::text, '_', lpad((p.numero)::text, 5, '0'::text), '_', p.suffixe)
+            WHEN ((p.suffixe IS NULL) AND (v.code_fantoir IS NULL)) THEN concat(c.insee_code, '_', 'xxxx', '_', lpad((p.numero)::text, 5, '0'::text))
+            WHEN ((p.suffixe IS NULL) AND (v.code_fantoir IS NOT NULL)) THEN concat(c.insee_code, '_', (v.code_fantoir)::text, '_', lpad((p.numero)::text, 5, '0'::text))
+            WHEN ((p.suffixe IS NOT NULL) AND (v.code_fantoir IS NULL)) THEN concat(c.insee_code, '_', 'xxxx', '_', lpad((p.numero)::text, 5, '0'::text), '_', p.suffixe)
+            ELSE NULL::text
+        END AS cle_interop,
+    c.insee_code AS commune_insee,
+    c.commune_nom,
+    cd.insee_code AS commune_deleguee_insee,
+    cd.commune_deleguee_nom,
+    v.nom_complet AS voie_nom,
+    p.lieudit_complement_nom,
+    p.numero,
+    p.suffixe,
+    p.type_pos AS "position",
+    public.st_x(p.geom) AS x,
+    public.st_y(p.geom) AS y,
+    round((public.st_x(public.st_transform(p.geom, 4326)))::numeric, 10) AS long,
+    round((public.st_y(public.st_transform(p.geom, 4326)))::numeric, 10) AS lat,
+    g.id AS cad_parcelles,
+    c.commune_nom AS source,
+    p.date_modif AS date_der_maj
+   FROM (((((adresse.point_adresse p
+     LEFT JOIN adresse.commune c ON ((p.id_commune = c.id_com)))
+     LEFT JOIN adresse.referencer_com rc ON ((p.id_commune = rc.id_com)))
+     LEFT JOIN adresse.commune_deleguee cd ON ((cd.id_com_del = rc.id_com_deleguee) AND St_Contains(cd.geom, p.geom)))
+     LEFT JOIN adresse.voie v ON ((p.id_voie = v.id_voie)))
+     LEFT JOIN adresse.parcelle g ON ((p.id_parcelle = g.fid)))
+UNION
+ SELECT ''::text AS uid_adresse,
+    concat(c.insee_code, '_', 'xxxx', '_', ld.numero) AS cle_interop,
+    c.insee_code AS commune_insee,
+    c.commune_nom,
+    cd.insee_code AS commune_deleguee_insee,
+    cd.commune_deleguee_nom,
+    ld.nom_ld AS voie_nom,
+    ''::character varying(255) AS lieudit_complement_nom,
+    ld.numero,
+    ''::text AS suffixe,
+    ''::text AS "position",
+    public.st_x(ld.geom) AS x,
+    public.st_y(ld.geom) AS y,
+    round((public.st_x(public.st_transform(ld.geom, 4326)))::numeric, 10) AS long,
+    round((public.st_y(public.st_transform(ld.geom, 4326)))::numeric, 10) AS lat,
+    ''::text AS cad_parcelles,
+    c.commune_nom AS source,
+    ld.date_der_maj
+   FROM (((adresse.lieux_dits ld
+     LEFT JOIN adresse.commune c ON ((c.id_com = ld.id_com)))
+     LEFT JOIN adresse.referencer_com rc ON ((ld.id_com = rc.id_com)))
+     LEFT JOIN adresse.commune_deleguee cd ON ((cd.id_com_del = rc.id_com_deleguee) AND St_Contains(cd.geom, ld.geom)))
+  WHERE (ld.integration_ban = true);
+
 COMMIT;
